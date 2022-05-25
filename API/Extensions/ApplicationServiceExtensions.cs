5e8c940909e4d9ff326afa75405cb90047f64d65
syf66666 <shao.yifan@northeastern.eud>
2022-05-24 23:44:48 -0400

    with ts register page

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -12,6 +12,11 @@ namespace API.Extensions
     public static class ApplicationServiceExtensions
     {
         //TODO:how to use this in csharp
+        //usually static class cant use this, except extension method
+        //what is extension method? 
+        //It is a new approach or concept that has been added in C#3.0 version 
+        //which allows to add new methods in an existing class without editing the source of the class.
+ 
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config){
             //different lifetime
             //AddSingleTon(existed until the app stop), AddTransient(existed until the method finished)
--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 
+
 namespace API.Extensions
 {
     public static class IdentityServiceExtensions
